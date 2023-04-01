08837545f0f0615b9a68dde43fae994b9d96a25b
Adarsh Sulegai <adarshsulegai50@gmail.com>
2023-04-01 15:12:23 +0530

    add vue

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from 'ui';
+import { Component } from 'react-ui';
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <Component />
     </div>
   );
 }
